6efe0adef45e1b009c0c7b7f43c1eb1869a52127
Daniel Massud <danielmassud@outlook.com>
2024-02-22 18:18:20 -0300

    retirando o r1.close que estava bugando o terminalhud quando finalizava o gcc.check()

--- core/MenuCLI/Requirements/GCC.js
+++ core/MenuCLI/Requirements/GCC.js
@@ -54,7 +54,7 @@ class GCC {
 
             return new Promise(resolve => {
                 rl.question('Press any key to continue...', () => {
-                    rl.close();
+                    
                     resolve();
                 });
             });
