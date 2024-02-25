0848de18b23cf9f522c8a1649e5aa780a574a8ca
Daniel Massud <danielmassud@outlook.com>
2024-02-24 22:51:27 -0300

    Criação do Menu de personalização do server no MenuCLI, inserida a primeira opção que define se vai rodar/buildar com o CUDA ou não

--- core/MenuCLI/ServerMenu.js
+++ core/MenuCLI/ServerMenu.js
@@ -2,6 +2,50 @@ import EasyAI from '../../EasyAI.js'
 import StartMenu from './StartMenu.js'
 import MenuCLI from './MenuCLI.js'
 
+let easyai_config = {}
+
+const CustomServer = () => ({
+    title : `• EasyAI Server | Configurar Server
+`,
+options : [
+    {
+    name : '⚡ Iniciar Servidor ⚡',
+    action : () => {
+        if(easyai_config == {}){
+            let server = new EasyAI.Server()
+            server.start()
+        } else {
+            let server = new EasyAI.Server(easyai_config)
+            server.start()
+        }
+    }
+    },
+    {
+    name : `CUDA ${easyai_config.llama ? (easyai_config.llama.cuda ? '✔️' : '❌') : '❌'}`,
+    action : () => {
+                if(easyai_config.llama){
+                    if(easyai_config.llama.cuda){
+                        easyai_config.llama.cuda = false
+                    } else {
+                        easyai_config.llama.cuda = true
+                    }
+                } else {
+                    easyai_config.llama = {}
+                    easyai_config.llama.cuda = true
+                }
+                MenuCLI.displayMenu(CustomServer)
+            }
+    },
+    {
+        name : '← Voltar',
+        action : () => {
+            MenuCLI.displayMenu(ServerMenu)
+            }
+        }
+     ]
+
+})
+
 const ServerMenu = () => ({
     title : `• EasyAI Server •
 `,
@@ -16,7 +60,8 @@ options : [
     {
     name : '✏️ Inicio Personalizado',
     action : () => {
-
+        easyai_config = {}
+        MenuCLI.displayMenu(CustomServer)
         }
     },
     {
