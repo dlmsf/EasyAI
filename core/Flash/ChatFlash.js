af2bd79610eb85889f5c0edb26d677cc0c7e0391
Daniel Massud <danielmassud@outlook.com>
2024-06-02 20:52:05 -0300

    ChatFLash 100%, subindo um EasyAI server com as configurações padrões

--- core/Flash/ChatFlash.js
+++ core/Flash/ChatFlash.js
@@ -4,7 +4,9 @@ import EasyAI from "../../EasyAI.js"
 import Chat from "../ChatModule/Chat.js"
 import TerminalChat from "../TerminalChat.js"
 import ChatPrompt from "../MenuCLI/Sandbox/ChatPrompt.js"
+import PM2 from "../useful/PM2.js"
 
+await EasyAI.Server.PM2()
 let ai = new EasyAI({server_url : 'localhost',server_port : 4000})
 const chat = new Chat()
 console.clear()
@@ -17,4 +19,6 @@ console.clear()
             })
             let result = await ai.Generate(`${ChatPrompt}${historical_prompt}AI:`,{tokenCallback : async (token) => {await displayToken(token.stream.content)},stop : ['|']})
             chat.NewMessage('AI: ',result.full_text)
-        })
+        },{exitFunction : async () => {
+            await PM2.Delete('pm2_easyai_server')
+        }})
