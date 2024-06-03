f88af1bdabe11529c7e3eeaa33eacfb5443c3773
Daniel Massud <danielmassud@outlook.com>
2024-06-03 12:59:43 -0300

    GenerateFlash com possibilidade de um save `generate save_name`

--- core/Flash/GenerateFlash.js
+++ core/Flash/GenerateFlash.js
@@ -4,8 +4,31 @@ import EasyAI from "../../EasyAI.js"
 import TerminalGenerate from "../TerminalGenerate.js"
 import PM2 from "../useful/PM2.js"
 
-await EasyAI.Server.PM2()
-let ai = new EasyAI({server_url : 'localhost',server_port : 4000})
+let ai
+
+const args = process.argv.slice(2);
+
+if (args.length > 0) {
+    await ServerSaves.Load(args[0])
+    .then(async (save) => {
+
+            await EasyAI.Server.PM2({token : save.Token,port : save.Port,EasyAI_Config : save.EasyAI_Config})
+            console.log('✔️ PM2 Server iniciado com sucesso !')
+            ai = new EasyAI({server_url : 'localhost',server_port : 4000})
+
+    }).catch(async e => {
+
+        console.log(`Save ${ColorText.red(args[0])} não foi encontrado`)
+        await EasyAI.Server.PM2()
+        ai = new EasyAI({server_url : 'localhost',server_port : 4000})
+   
+    })
+} else {
+    await EasyAI.Server.PM2()
+    ai = new EasyAI({server_url : 'localhost',server_port : 4000})
+}
+
+
 console.clear()
 
         new TerminalGenerate(async (input,display) => {
