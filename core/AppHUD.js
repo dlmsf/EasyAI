47bc3a84f5e01199f0e5fe6c2603717516cdd750
Daniel Massud 2 <massudstudio@gmail.com>
2025-09-08 15:26:38 -0400

    base do AppHUD

---
+++ core/AppHUD.js
@@ -0,0 +1,21 @@
+
+
+class AppHUD_Func {
+    constructor(){
+
+    }
+}
+
+class AppHUD {
+
+constructor(){
+this.Tree = []
+
+
+}
+
+static Func(){return AppHUD_Func}
+
+}
+
+export default AppHUD
