89276b3499b418537ded75f9bfed698360d7588e
Daniel Massud <massudstudio@gmail.com>
2026-02-23 12:38:34 -0500

    07 init

---
+++ core/0.7.0/EasyAI.js
@@ -0,0 +1,21 @@
+class EasyAI {
+
+static Test(){
+  
+}
+ 
+static Generate(){
+
+}
+
+static Chat(){
+
+
+}
+
+}
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+    console.log('teste')
+  }
+  
